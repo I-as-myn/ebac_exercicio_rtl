afbaa18ddb9a45ddcab1d5711b800a1b27411259
Iasmyn <eng.iasmyn@gmail.com>
2025-01-08 19:04:58 -0300

    teste para inserção de dois comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
@@ -7,4 +6,19 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+    it('Deve permitir a inserção de dois comentários', () => {
+        render(<PostComment />);
+
+        const textarea = screen.getByTestId('comment-input');
+        const button = screen.getByTestId('submit-button');
+
+        fireEvent.change(textarea, {target: {value:'Primeiro comentário'}});
+        fireEvent.click(button);
+
+        fireEvent.change(textarea, {target: {value:'Segundo comentário'}});
+        fireEvent.click(button);
+
+        expect(screen.getByTestId('comment-0')).toHaveTextContent('Primeiro comentário');
+        expect(screen.getByTestId('comment-1')).toHaveTextContent('Segundo comentário');
+    })
 });
